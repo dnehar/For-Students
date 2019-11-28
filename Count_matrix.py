0f7b18d7c97af9404dfb857aaf2f2e9d53b5f023
Djamel NEHAR-BELAID <djamel.nehar-belaid@jax.org>
2019-11-27 19:05:48 -0500

    Update Count_matrix.py

--- Count_matrix.py
+++ Count_matrix.py
@@ -1,8 +1,11 @@
+#--- save ount matrix (highly variable genes) .csv file 
+adata.X.T.write_csvs(...)
 
+#----  save raw counts .csv file
+adata.raw.X.T.write_csvs(...)
 
 
 #--- from AnnData to 10X 
-
 pd.DataFrame(ad.var.index).to_csv(os.path.join(destination, "genes.tsv" ),   sep = "\t", index_col = False)
 pd.DataFrame(ad.obs.index).to_csv(os.path.join(destination, "barcodes.tsv"), sep = "\t", index_col = False)
 ad.obs.to_csv(os.path.join(destination, "metadata.tsv"), sep = "\t", index_col = True)
